2cb5b6dfc2ccbf197cde2fb798c03997481e9ae1
Bartłomiej Zając <bartoss97@gmail.com>
2022-09-21 01:47:42 +0200

    Add 3D models reading lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use tobj;
+
 fn main() {
-    println!("Hello, world!");
+    let obj_file = tobj::load_obj("resources/teapot.obj", &tobj::GPU_LOAD_OPTIONS);
+    assert!(obj_file.is_ok());
+    let (models, _materials) = obj_file.expect("Failed to load OBJ file");
+    println!("# of models: {}", models.len());
 }
