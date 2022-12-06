8faf82498f6fcd1f36279f2b125a8cf3f84e4792
Bartłomiej Zając <bartoss97@gmail.com>
2022-12-06 23:11:52 +0100

    Log to one file in debug build

--- src/logger.rs
+++ src/logger.rs
@@ -1,19 +1,26 @@
+use std::{ io::Write };
+
 pub fn init() {
     use simplelog::*;
 
     if !std::path::Path::new("logs/").is_dir() {
         std::fs::create_dir("logs").unwrap();
     }
+
     let log_file_config = ConfigBuilder::new().set_time_format_rfc3339().build();
     let timestamp = chrono::Utc::now().format("%Y-%m-%dT%H%M%SZ").to_string();
-    let log_file_path = if cfg!(debug_assertions) {
-        format!("logs/debug_{}.log", timestamp)
-    } else {
-        format!("logs/{}.log", timestamp)
-    };
-    let log_file = std::fs::File::create(log_file_path).unwrap();
 
     if cfg!(debug_assertions) {
+        let log_file_path = String::from("logs/debug.log");
+        let mut log_file = std::fs::OpenOptions
+            ::new()
+            .write(true)
+            .create(true)
+            .append(true)
+            .open(log_file_path)
+            .unwrap();
+        writeln!(log_file, "--\nStarted at {}\n--", timestamp).expect("Unabled to write to debug log file");
+
         CombinedLogger::init(
             vec![
                 TermLogger::new(LevelFilter::Trace, Config::default(), TerminalMode::Mixed, ColorChoice::Auto),
@@ -21,6 +28,9 @@ pub fn init() {
             ]
         ).unwrap();
     } else {
+        let log_file_path = format!("logs/{}.log", timestamp);
+        let log_file = std::fs::File::create(log_file_path).unwrap();
+
         CombinedLogger::init(
             vec![
                 TermLogger::new(LevelFilter::Warn, Config::default(), TerminalMode::Mixed, ColorChoice::Auto),
